a0d01ed3e3f800c64b88f3d44ea2c6f6e1e65760
Jordan Peters <rollersteaam@googlemail.com>
2020-01-25 22:33:43 +0000

    Add example filter query

--- website/cycloneapp/views.py
+++ website/cycloneapp/views.py
@@ -6,7 +6,12 @@ from .models import Cyclone
 def index(request):
     context = {}
     cyclones = Cyclone.objects.all()
+
+    # 1950 and 1970
+    ranged_cyclones = Cyclone.objects.filter(date__range=["1950-01-01", "1970-01-01"])
+    
     context["cyclones"] = cyclones
+
     return render(request, "cycloneapp/index.html", context)
 
 def upload_cyclones(request):
