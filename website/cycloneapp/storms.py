049d43c2272c514725549d591dc9908439638f60
rollersteaam <rollersteaam@googlemail.com>
2020-01-27 13:08:25 +0000

    Improve perform. logs, remove input file param query

--- website/cycloneapp/storms.py
+++ website/cycloneapp/storms.py
@@ -15,25 +15,39 @@ import pathlib
 # radius = float(sys.argv[3])
 # input_file = sys.argv[4]
 
-def storm_query_slow(latitude, longitude, radius, start_year, end_year, input_file):
-    # Populate cyclone nodes.
-    nodes = 0
-    # max_nodes = 10000
-    max_nodes = 10000000
-    cyclone_nodes = []
+# Sorry for the globals Lewis xD
+cyclone_nodes = []
+nodes = 0
+max_nodes = 10000000
+input_file_path = "cycloneapp/static/globe/Real-Cyclones-Intense.csv"
 
-    print(pathlib.Path().absolute())
+print(f"Loading cyclone nodes from {input_file_path} into memory...")
 
-    file = open(input_file, 'r')
+with open(input_file_path, 'r') as file:
     for line in file:
         cyclone_nodes.append(line.strip().split(','))
-        if nodes > max_nodes:
-            print("WARNING: Input has over", max_nodes, "nodes. Only read first", max_nodes, "cyclone nodes.")
-            break
-        nodes = nodes + 1
-    cyclone_nodes.pop(0)
 
-    # Dictionary mapping cyclones to a list of their cyclone nodes.
+        if nodes > max_nodes:
+            print(f"WARNING: Cyclone nodes file has over {max_nodes} nodes. Only the first {max_nodes} will be read and used.")
+            break
+
+        nodes = nodes + 1
+
+cyclone_nodes.pop(0)
+
+print("Cyclone nodes loaded!")
+
+query_id = 1
+
+def storm_query_slow(latitude, longitude, radius, start_year, end_year):
+    print(f"Query {query_id} received: {latitude} {longitude} {radius} {start_year} {end_year}")
+
+    # Start timer.
+    start = time.time()
+
+    # Dictionary mapping cyclones to a list of their cyclone nodes
+    print("Mapping cyclones to their cyclone nodes...")
+
     count = 0
     cyclones = {}
     cyclone = []
@@ -47,10 +61,13 @@ def storm_query_slow(latitude, longitude, radius, start_year, end_year, input_fi
             cyclone = [[float(node[3]), float(node[4]), float(node[6])]]
             last = current
 
-    # Start timer.
-    start = time.time()
+    mapping_end = time.time()
+    seconds = round(mapping_end - start, 2)
+    print(f"Time elapsed for mapping: {seconds} seconds")
 
-    # Create a list of SIDs that have at least one node within boundaries.
+    # Create a list of SIDs that have at least one node within boundaries
+    print("Finding all cyclones that have at least one node within target boundary...")
+
     SIDs_in_radius = ['']
     for node in cyclone_nodes:
         year = datetime.date(int(node[5][:4]), 1, 1)
@@ -69,23 +86,37 @@ def storm_query_slow(latitude, longitude, radius, start_year, end_year, input_fi
                 count = count + 1
     SIDs_in_radius.pop(0)
 
-    # Filter the valid cyclones by SID and add to JSON.
+    boundaries_end = time.time()
+    seconds = round(boundaries_end - mapping_end, 2)
+    print(f"Time elapsed for boundary checking: {seconds} seconds")
+
+    print("Filtering only valid cyclones...")
+
+    # Filter the valid cyclones by SID and add to JSON
     filtered_cyclones = {}
     for SID in SIDs_in_radius:
         filtered_cyclones[SID] = cyclones[SID]
 
+    filter_end = time.time()
+    seconds = round(filter_end - boundaries_end, 2)
+    print(f"Time elapsed for filtering: {seconds} seconds")
+
+    # Output final performance results
+    end = time.time()
+    seconds = round(end - start, 2)
+    print("Time elapsed:", seconds, "seconds")
+
+    location = "(" + str(latitude) + ", " + str(longitude) + ")."
+    print(f"{count} cyclones have at least one node within {radius} kilometres of {location}")
+    
+    print(f"Query {query_id} complete!")
+    query_id += 1
+
     return filtered_cyclones
+
     # lines = json.dumps(filtered_cyclones, sort_keys=True, indent=4, separators=(',', ': '))
     # file = open('output.json', 'w')
     # file.truncate(0)
     # for line in lines:
     #     file.write(line)
     # file.close()
-
-    # End timer.
-    # end = time.time()
-    # seconds = round(end - start, 2)
-    # print("Time elapsed:", seconds, "seconds")
-
-    # location = "(" + str(latitude) + ", " + str(longitude) + ")."
-    # print(count, "cyclones have at least one node within", radius, "kilometres of", location)
